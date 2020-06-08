ef82741e5482d12b3b74a19975b2d34d3e457e0c
Miguel Iglesias <miguel.iglesias@dimeder.com>
2020-06-08 13:20:19 -0400

    options there

--- src/main/scala/toMatrix.scala
+++ src/main/scala/toMatrix.scala
@@ -11,15 +11,37 @@ import simulacrum._
   def extractPaths(c: C ): List[Symbol]
 }
 
-@typeclass trait IsLeaf[A] {
+@typeclass trait Leaf[A] {
   def leaf(a: A ): Symbol
 }
 
+object Leaf {
+  implicit def leafZ = new Leaf[Z] {
+    def leaf(z: Z ): Symbol = Symbol( z.i.toString )
+  }
+}
 trait ExtractPathsLow {
-// implicit def leaf[K <: Symbol, L](implicit key: Witness.Aux[K] ): ExtractPaths[FieldType[K, L]] =
-//   new ExtractPaths[FieldType[K, L]] {
-//     def extractPaths(c: FieldType[K, L] ): List[K] = List( key.value )
-//   }
+  implicit def leaf[K <: Symbol, L](implicit key: Witness.Aux[K], L: ExtractPaths[L] ): ExtractPaths[FieldType[K, L]] =
+    new ExtractPaths[FieldType[K, L]] {
+      def extractPaths(c: FieldType[K, L] ) = List( key.value ) ::: L.extractPaths( c )
+    }
+  implicit def optionLeaf[L, K <: Symbol](implicit key: Witness.Aux[K] ): ExtractPaths[FieldType[K, Option[L]]] =
+    new ExtractPaths[FieldType[K, Option[L]]] {
+      def extractPaths(cs: FieldType[K, Option[L]] ): List[Symbol] = cs.map( _ => key.value ).toList
+    }
+  implicit def hconsLeaf[K <: Symbol, H, T <: HList](
+      implicit
+      key: Witness.Aux[K],
+      tail: ExtractPaths[T]
+    ): ExtractPaths[FieldType[K, H] :: T] =
+    new ExtractPaths[FieldType[K, H] :: T] {
+      def extractPaths(c: FieldType[K, H] :: T ) = key.value :: tail.extractPaths( c.tail )
+    }
+  implicit def list[K <: Symbol, L](implicit key: Witness.Aux[K], L: ExtractPaths[L] ): ExtractPaths[FieldType[K, List[L]]] =
+    new ExtractPaths[FieldType[K, List[L]]] {
+      def extractPaths(c: FieldType[K, List[L]] ) = c.flatMap( L.extractPaths )
+    }
+
 }
 object ExtractPaths extends ExtractPathsLow {
   implicit def generic[A, G](implicit gen: LabelledGeneric.Aux[A, G], sg: Lazy[ExtractPaths[G]] ): ExtractPaths[A] =
@@ -28,6 +50,7 @@ object ExtractPaths extends ExtractPathsLow {
     }
   implicit def hnil: ExtractPaths[HNil] = new ExtractPaths[HNil] { def extractPaths(c: HNil ): List[Symbol] = Nil }
   implicit def cnil: ExtractPaths[CNil] = new ExtractPaths[CNil] { def extractPaths(c: CNil ): List[Symbol] = Nil }
+
   implicit def hcons[K <: Symbol, H, T <: HList](
       implicit
       key: Witness.Aux[K],
@@ -37,28 +60,29 @@ object ExtractPaths extends ExtractPathsLow {
     new ExtractPaths[FieldType[K, H] :: T] {
       def extractPaths(c: FieldType[K, H] :: T ) = head.extractPaths( c.head ) ::: tail.extractPaths( c.tail )
     }
-  implicit def leaf[K <: Symbol, L](implicit key: Witness.Aux[K], L: ExtractPaths[L] ): ExtractPaths[FieldType[K, L]] =
-    new ExtractPaths[FieldType[K, L]] {
-      def extractPaths(c: FieldType[K, L] ) = List( key.value ) ::: L.extractPaths( c )
+
+  implicit def leafList[K <: Symbol, L](implicit key: Witness.Aux[K], L: Leaf[L] ): ExtractPaths[FieldType[K, List[L]]] =
+    new ExtractPaths[FieldType[K, List[L]]] {
+      def extractPaths(c: FieldType[K, List[L]] ) = c.map( L.leaf )
     }
-// implicit def list[L](implicit L: IsLeaf[L] ): ExtractPaths[List[L]] =
-//   new ExtractPaths[List[L]] {
-//     def extractPaths(cs: List[L] ): List[Symbol] = cs.map( L.leaf )
-//   }
-// implicit def option[L](implicit L: IsLeaf[L] ): ExtractPaths[Option[L]] =
-//   new ExtractPaths[Option[L]] {
-//     def extractPaths(cs: Option[L] ): List[Symbol] = cs.map( L.leaf ).toList
-//   }
+
+  implicit def option[L, K <: Symbol](implicit key: Witness.Aux[K], L: ExtractPaths[L] ): ExtractPaths[FieldType[K, Option[L]]] =
+    new ExtractPaths[FieldType[K, Option[L]]] {
+      def extractPaths(cs: FieldType[K, Option[L]] ): List[Symbol] = cs.toList.map( _ => key.value ) ::: cs.toList.flatMap( L.extractPaths )
+    }
+
 }
 
 case class X()
-case class Y()
-case class Z()
-case class B(x: X )
-case class C(y: Y, z: Z )
+case class Y(i: Int )
+case class Z(i: Int )
+case class B(y: Option[Y], x: X )
+case class C(z: List[Z], y: Y )
 case class A(b: B, c: C )
 
 object Main extends App {
-  val a = A( B( X() ), C( Y(), Z() ) )
-  pprint.pprintln( ExtractPaths[A].extractPaths( a ) )
+  val a1 = A( B( None, X() ), C( List( Z( 1 ), Z( 2 ) ), Y( 1 ) ) )
+  val a2 = A( B( Some( Y( 3 ) ), X() ), C( List(), Y( 2 ) ) )
+  pprint.pprintln( ExtractPaths[A].extractPaths( a1 ) )
+  pprint.pprintln( ExtractPaths[A].extractPaths( a2 ) )
 }
