6bf9d32c9315c4a6be340a2c50f40310ffc29271
Miguel Iglesias <miguel.iglesias@dimeder.com>
2020-06-27 11:17:44 -0400

    cute

--- src/main/scala/graph.scala
+++ src/main/scala/graph.scala
@@ -8,19 +8,6 @@ import scala.language.implicitConversions
 import scala.language.higherKinds
 import cats.{ Applicative, Eq }
 import cats.implicits._
-/*
-sealed trait IGraph[I, G, O]
-object IGraph {
-  case class Nodes[I, G, O](graph: G ) extends IGraph[I, G, O]
-  object Nodes {
-    def node[A](a: A ): IGraph[_1, A, _1] = IGraph( a )
-  }
-  case class Empty[I, G, O]() extends IGraph[I, G, O]
-  object Empty {
-    def empty[A]: Empty[_0, A, _0] = Empty()
-  }
-}
- */
 
 case class IGraph[I, G, O](graph: G )
 object IGraph {
@@ -63,20 +50,6 @@ object Juxtapose extends JuxtaposeBase {
       type Out = Connector.Vertical[A, Connector.Vertical[B, C]]
       def juxtapose(g1: Connector.Vertical[A, B], g2: C ) = Connector.Vertical( g1.north, Connector.Vertical( g1.south, g2 ) )
     }
-  //implicit def verticalRight[A, B, C, JA, JB](implicit ca: Juxtapose.Aux[C, A, JA], cb: Juxtapose.Aux[C, B, JB], cab: Juxtapose[JA, JB] ) = new Juxtapose[C, Connector.Vertical[A, B]] {
-  //  type Out = cab.Out
-  //  def juxtapose(g1: C, g2: Connector.Vertical[A, B] ): Out = cab.juxtapose( ca.juxtapose( g1, g2.north ), cb.juxtapose( g1, g2.south ) )
-  //}
-  //implicit def emptyLeft[A, B, IA <: Nat, OA <: Nat, IB <: Nat, OB <: Nat, C] =
-  //  new Juxtapose[IGraph.Empty[IA, A, OA], IGraph[IB, B, OB]] {
-  //    type Out = IGraph[IA, A, OA]
-  //    def juxtapose(g1: IGraph.Empty[IA, A, OA], g2: IGraph[IB, B, OB] ): IGraph[IA, A, OA] = g1
-  //  }
-  //implicit def emptyRight[A, B, IA <: Nat, OA <: Nat, IB <: Nat, OB <: Nat, C] =
-  //  new Juxtapose[IGraph[IA, A, OA], IGraph.Empty[IB, B, OB]] {
-  //    type Out = IGraph[IB, B, OB]
-  //    def juxtapose(g1: IGraph[IA, A, OA], g2: IGraph.Empty[IB, B, OB] ): IGraph[IB, B, OB] = g2
-  //  }
 }
 trait ConcatBase {
   implicit def prod[A, B] = new Concat[A, B] {
@@ -105,7 +78,6 @@ trait Graph[N[_]] {
       SI: Sum[IA, IB],
       SO: Sum[OA, OB]
     ): N[IGraph[SI.Out, C, SO.Out]]
-  //def empty[A](a: N[A] ): N[IGraph.Empty[_0, A, _0]]
 }
 
 object test extends App {
@@ -124,22 +96,13 @@ object test extends App {
     def juxtapose(g1: Int, g2: Int ) = Connector.Vertical( g1, g2 )
   }
   val g12: IGraph[_2, Connector.Vertical[Int, Int], _2] = g1.juxtapose( g2 )
-  pprint.pprintln( g3.juxtapose( g12 ) )
-  //val a: Connector.Vertical[IGraph[_2, Connector.Vertical[Int, Int], _2], IGraph[_1, Int, _1]] = g1.juxtapose( g2 ).juxtapose( g3 )
-  //val b: IGraph[_2, Connector.Vertical[Int, Int], _2] = g1.juxtapose( g2 )
-  val c: IGraph[_3, Connector.Vertical[Int, Connector.Vertical[Int, Int]], _3] = g1.juxtapose( g2 ).juxtapose( g3 )
-  //pprint.pprintln( g1.juxtapose( g2 ).juxtapose( g3 ) )
-  //pprint.pprintln( g1.concat( g2 ) )
-// implicit def eqR[A]: Eq[R[A]] = Eq.fromUniversalEquals
-// implicit def graphR = new Graph[R] {
-//   def node[A](a: A ): R[A] = ???
-//   def edge[A, B](start: R[A], end: R[B] ): R[Edge[A, B]] = ???
-//   def join[A, B, C](edge: Edge[A, B], node: R[C] ): R[Join[A, B, C]] = ???
-//   def empty[A](a: R[A] ): R[Empty[A]] = ???
-// }
-// val RG = Graph[R]
-// import RG._
-// import Graph.ops._
-// val g1 = node( 1 ).edge( node( 2 ) ).edge( node( 3 ) )
-// val g2 = node( 1 ).edge( node( 2 ) ).join( node( 3 ) )
+  val g123: IGraph[_3, Connector.Vertical[Int, Connector.Vertical[Int, Int]], _3] = g12.juxtapose( g3 )
+  val g312: IGraph[_3, Connector.Vertical[Int, Connector.Vertical[Int, Int]], _3] = g3.juxtapose( g12 )
+  val g1231: IGraph[_4, Connector.Vertical[Int, Connector.Vertical[Connector.Vertical[Int, Int], Int]], _4] = g12.juxtapose( g3 ).juxtapose( g1 )
+  val g1123: IGraph[_4, Connector.Vertical[Int, Connector.Vertical[Int, Connector.Vertical[Int, Int]]], _4] = g1.juxtapose( g12.juxtapose( g3 ) )
+  pprint.pprintln( g1 )
+  pprint.pprintln( g123 )
+  pprint.pprintln( g312 )
+  pprint.pprintln( g1231 )
+  pprint.pprintln( g1123 )
 }
