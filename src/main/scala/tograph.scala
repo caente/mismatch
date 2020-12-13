767e4e07e1236ee094ae89ae69057b8c53135824
Miguel Iglesias <miguel.iglesias@dimeder.com>
2020-12-13 18:48:21 -0500

    no commented code

--- src/main/scala/tograph.scala
+++ src/main/scala/tograph.scala
@@ -58,87 +58,6 @@ case class Index[A](i: Int ) extends Labelled[A]
 trait ToGraph[C, G[_], Label] {
   def toGraph(parent: NonEmptyList[Label], c: C ): G[Label] => G[Label]
 }
-
-/*
-trait Bottom {
-  implicit def cconsNot[H, K <: Symbol, T <: HList, G[_]](
-      implicit
-      key: Witness.Aux[K],
-      A: Connect[G],
-      C: Show[H],
-      N: ToGraph[T, G, Labelled.AsString]
-    ) = new ToGraph[FieldType[K, H] :: T, G, Labelled.AsString] {
-    def toGraph(
-        parent: NonEmptyList[Labelled.AsString],
-        c: FieldType[K, H] :: T
-      ): G[Labelled.AsString] => G[Labelled.AsString] = { graph =>
-      val label: Labelled.AsString = Node( key.value.toString.drop( 1 ) )
-      val graphWithLabel = A.connect( graph )( parent, label )
-      val graphWithLeaf = A.connect( graphWithLabel )( label :: parent, Leaf( Show[H].show( c.head ) ) )
-      N.toGraph( parent, c.tail )( graphWithLeaf )
-    }
-  }
-}
-
-trait Hlists extends Bottom {
-  implicit def ccons[H, K <: Symbol, T <: HList, G[_]](
-      implicit
-      key: Witness.Aux[K],
-      A: Connect[G],
-      C: ToGraph[H, G, Labelled.AsString],
-      N: ToGraph[T, G, Labelled.AsString]
-    ) = new ToGraph[FieldType[K, H] :: T, G, Labelled.AsString] {
-    def toGraph(
-        parent: NonEmptyList[Labelled.AsString],
-        c: FieldType[K, H] :: T
-      ): G[Labelled.AsString] => G[Labelled.AsString] = { graph =>
-      N.toGraph( parent, c.tail )(
-        C.toGraph( Node[String]( key.value.toString.drop( 1 ) ) :: parent, c.head )(
-          A.connect( graph )( parent, Node( key.value.toString.drop( 1 ) ) )
-        )
-      )
-    }
-  }
-
-  implicit def hnil[G[_]] = new ToGraph[HNil, G, Labelled.AsString] {
-    def toGraph(parent: NonEmptyList[Labelled.AsString], c: HNil ): G[Labelled.AsString] => G[Labelled.AsString] =
-      identity
-  }
-  implicit def cnil[G[_]]: ToGraph[CNil, G, Labelled.AsString] = new ToGraph[CNil, G, Labelled.AsString] {
-    def toGraph(parent: NonEmptyList[Labelled.AsString], c: CNil ): G[Labelled.AsString] => G[Labelled.AsString] =
-      identity
-  }
-
-  implicit def coproduct[G[_], H, T <: Coproduct, K <: Symbol](
-      implicit
-      key: Witness.Aux[K],
-      C: ToGraph[H, G, Labelled.AsString],
-      N: ToGraph[T, G, Labelled.AsString]
-    ): ToGraph[FieldType[K, H] :+: T, G, Labelled.AsString] =
-    new ToGraph[FieldType[K, H] :+: T, G, Labelled.AsString] {
-      def toGraph(
-          parent: NonEmptyList[Labelled.AsString],
-          c: FieldType[K, H] :+: T
-        ): G[Labelled.AsString] => G[Labelled.AsString] = { graph =>
-        c match {
-          case Inl( h ) =>
-            C.toGraph( parent, h )( graph )
-          case Inr( tail ) =>
-            N.toGraph( parent, tail )( graph )
-        }
-      }
-    }
-  implicit def generic[P, C, G[_]](
-      implicit
-      gen: LabelledGeneric.Aux[P, C],
-      G: Lazy[ToGraph[C, G, Labelled.AsString]]
-    ): ToGraph[P, G, Labelled.AsString] = new ToGraph[P, G, Labelled.AsString] {
-    def toGraph(parent: NonEmptyList[Labelled.AsString], p: P ): G[Labelled.AsString] => G[Labelled.AsString] =
-      G.value.toGraph( parent, gen.to( p ) )
-  }
-
-}
- */
 trait Bottom {
   implicit def fromShow[A, G[_]](implicit A: Show[A], G: Connect[G] ) = new ToGraph[A, G, Labelled[String]] {
     def toGraph(parent: NonEmptyList[Labelled[String]], c: A ): G[Labelled[String]] => G[Labelled[String]] = { graph =>
@@ -211,18 +130,6 @@ trait ToGraphInstances[G[_]] extends Magnolia[G] {
           ._1
     }
   }
- //implicit def listShow[P, G[_]](
- //    implicit
- //    A: Connect[G],
- //    S: Show[P]
- //  ): ToGraph[List[P], G, Labelled.AsString] = new ToGraph[List[P], G, Labelled.AsString] {
- //  def toGraph(parent: NonEmptyList[Labelled.AsString], c: List[P] ): G[Labelled.AsString] => G[Labelled.AsString] = {
- //    graph =>
- //      c.foldLeft( graph ) { ( graph, p ) =>
- //        A.connect( graph )( parent, Leaf( p.show ) )
- //      }
- //  }
- //}
   implicit def mapGraph[K: Ordering, V, G[_]](
       implicit
       A: Connect[G],
